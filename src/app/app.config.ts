7fdb60a8bbff8757edee4aed151ae5f422190c58
juandavidcertuche93 <juandavidcertuchequijano2@gmail.com>
2025-04-28 13:49:20 -0500

    pdate Angular configuration and setup environment files, Configure Tailwind CSS support

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,32 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+
+//Interceptor
+// import {injectSessionInterceptor} from "./core/interceptors/inject-session.interceptor"
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+    providers: [
+      provideZoneChangeDetection({ eventCoalescing: true }),
+      provideRouter(routes),
+      provideHttpClient(withInterceptors([ ]), withFetch()),
+      provideAnimationsAsync(),
+      providePrimeNG({
+        theme: {
+            preset: Aura,
+            options: {
+              cssLayer: {
+                  name: 'primeng',
+                  order: 'tailwind-base, primeng, tailwind-utilities'
+              }
+          }
+        }
+    }), provideAnimationsAsync()
+  ]
 };
+
---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,9 @@
+/*  aplicación Angular y ayudan a mantener las diferencias entre entornos de desarrollo
+    y producción de manera más organizada y controlada.
+*/
+
+export const environment = {
+  production: true,
+  API_URL: 'https://tu-api-de-produccion.com/api/'
+
+};
